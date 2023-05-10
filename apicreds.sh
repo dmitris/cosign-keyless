fc432b46e30cb5116d1a7ed22371f47ed874b463
Dmitry S <dsavints@gmail.com>
2023-05-10 13:55:00 +0200

    bash var check

--- apicreds.sh
+++ apicreds.sh
@@ -14,7 +14,7 @@ declare ROLE
 # ROLE="dummy_role_pusher" # optional - normally not needed!
 UNAME=$(uname)
 
-if [[ -z "$ROLE" ]]; then
+if [[ -z "${ROLE:-}" ]]; then
 	zts-roletoken -domain cd.docker.registry -svc-cert-file ~/.athenz/cert -svc-key-file ~/.athenz/key -zts https://zts.athens.yahoo.com:4443/zts/v1 > roletoken.txt
 else
 	zts-roletoken -role dummy_role_pusher -domain cd.docker.registry -svc-cert-file ~/.athenz/cert -svc-key-file ~/.athenz/key -zts https://zts.athens.yahoo.com:4443/zts/v1 > roletoken.txt
@@ -26,5 +26,6 @@ else
 	rtkn64=$(echo -n "user.${USER}:$rtkn" | base64 --wrap 0)
 fi
 jwt=$(curl -fsL -H "Authorization: Basic $rtkn64" "https://docker.ouroath.com:4443/token?service=docker.ouroath.com&scope=repository:${IMAGE}:${SCOPE}" | jq -r .token)
-# echo $jwt
+echo $jwt
 curl -H "Authorization: Bearer $jwt" https://docker.ouroath.com:4443/v2/$IMAGE/tags/list
+echo ""
