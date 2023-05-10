f404f2e2bcb1755a673c8f32aba45e225e136b78
Dmitry Savintsev <dsavints@gmail.com>
2023-05-10 09:48:25 +0000

    zts-podman.sh

---
+++ zts-podman.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+zts-roletoken -role dummy_role_pusher -domain cd.docker.registry -svc-cert-file ~/.athenz/cert -svc-key-file ~/.athenz/key -zts https://zts.athens.yahoo.com:4443/zts/v1 > roletoken.txt
+sudo podman login -u user.$USER -p $(cat roletoken.txt) docker.ouroath.com:4443
+
+# rm -fr ./service_cert.pem
+# zts-svccert -domain home.$USER -service  test0 -private-key ~/backup/misc/test0.key -key-version 0 -zts https://zts.athens.yahoo.com:4443/zts/v1 -dns-domain zts.yahoo.cloud -hdr Yahoo-Principal-Auth -cert-file ./service_cert.pem
+# docker login -u home.$USER.test0 -p $(cat roletoken.txt) docker.ouroath.com:4443
