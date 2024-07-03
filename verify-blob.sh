93cc652420fb54de3ff1991d12557f57669ca43b
Dmitry S <dsavints@gmail.com>
2024-07-03 17:47:31 +0200

    add script to sign/verify a blob

---
+++ verify-blob.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -euo pipefail
+
+## Requirements
+# - cosign
+# - crane
+# - go
+
+COSIGN=${COSIGN:="cosign"}
+TIMESTAMP_SERVER_URL=${TIMESTAMP_SERVER_URL:="https://freetsa.org/tsr"}
+TIMESTAMP_CERTCHAIN=${TIMESTAMP_CERTCHAIN:=""}
+if [[ "${TIMESTAMP_SERVER_URL}" == "https://freetsa.org/tsr" ]]; then
+	# download the TSA and TSA CA certificates and create the certificate chain file
+	rm -fr tsa.crt cacert.pem tsa-certchain.pem
+	curl -sSfL https://freetsa.org/files/tsa.crt -o tsa.crt
+	curl -sSfL https://freetsa.org/files/cacert.pem -o cacert.pem
+	cat cacert.pem tsa.crt > tsa-certchain.pem
+	echo "(PWD: $PWD) tsa-certchain.pem:"
+	cat tsa-certchain.pem
+	TIMESTAMP_CERTCHAIN="tsa-certchain.pem"
+fi
+if [[ -z "${TIMESTAMP_CERTCHAIN}" ]]; then
+	echo "TIMESTAMP_CERTCHAIN is not set and TIMESTAMP_SERVER_URL ${TIMESTAMP_SERVER_URL} is not default (https://freetsa.org/tsr)"
+	exit 1
+fi
+
+echo "TIMESTAMP_SERVER_URL: $TIMESTAMP_SERVER_URL, TIMESTAMP_CERTCHAIN: $TIMESTAMP_CERTCHAIN"
+
+GOBIN=/tmp GOPROXY=https://proxy.golang.org,direct go install -v github.com/dmitris/gencert@latest
+
+rm -f ca-key.pem key.pem
+# use gencert to generate CA, keys and certificates
+echo "generate keys and certificates with gencert"
+
+passwd=$(uuidgen | head -c 32 | tr 'A-Z' 'a-z')
+rm -f import-cosign.* && /tmp/gencert && COSIGN_PASSWORD="$passwd" cosign import-key-pair --key key.pem
+
+SIG_TIMESTAMP_FILE="sig-timestamp.txt"
+echo "cosign sign-blob:"
+COSIGN_PASSWORD="$passwd" ${COSIGN} sign-blob --timestamp-server-url "${TIMESTAMP_SERVER_URL}" \
+    --rfc3161-timestamp ${SIG_TIMESTAMP_FILE} --tlog-upload=false --key import-cosign.key README.md \
+	--output-signature README.md.sig
+
+# key is now longer needed
+rm -f key.pem import-cosign.*
+
+echo "cosign verify-blob (with --certificate-chain):"
+cosign verify-blob --private-infrastructure --insecure-ignore-sct \
+	--certificate-identity-regexp 'xyz@nosuchprovider.com' --certificate-oidc-issuer-regexp '.*' \
+	--certificate cert.pem --certificate-chain cacert.pem \
+	--rfc3161-timestamp ${SIG_TIMESTAMP_FILE} --timestamp-certificate-chain=$TIMESTAMP_CERTCHAIN \
+	--signature README.md.sig README.md
+echo "cosign verify-blob (with --ca-roots):"	
+cosign verify-blob --private-infrastructure --insecure-ignore-sct \
+	--certificate-identity-regexp 'xyz@nosuchprovider.com' --certificate-oidc-issuer-regexp '.*' \
+	--certificate cert.pem --ca-roots cacert.pem \
+	--rfc3161-timestamp ${SIG_TIMESTAMP_FILE} --timestamp-certificate-chain=$TIMESTAMP_CERTCHAIN \
+	--signature README.md.sig README.md
+
+# cleanup
+rm -f tsa-certchain.pem tsa.crt
